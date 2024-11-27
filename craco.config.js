26d0d366fcf7900d880840c6680e05bc4e75d35e
yxb <yexbxyz@163.com>
2024-11-27 10:32:29 +0800

    optimize: 新增打包资源分析

--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,15 @@
 const path = require('path')
 const resolve = dir =>  path.resolve(__dirname, dir)
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = {
   webpack: {
     alias: {
       "@": resolve("src"),
       "@utils": resolve("src/utils"),
-    }
+    },
+    plugins:[
+      new BundleAnalyzerPlugin({analyzerPort: 9090}),
+    ],
   }
 }
