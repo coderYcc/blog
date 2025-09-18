b1789868e2df2d20662fd1a3bc13381311165970
yxb <xiaobo.ye@hand-china.com>
2025-09-18 15:44:02 +0800

    add: 加载组件替换

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { renderRoutes } from 'react-router-config';
 import routes from './router';
 import { BrowserRouter } from 'react-router-dom';
 import { prepareRoutes } from './utils/route-loader';
-
+import CircleLoader from './components/loader'
 const App = memo(() => {
   const preparedRoutes = prepareRoutes(routes);
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={
+        <CircleLoader 
+          size="32px" 
+          primaryColor="#28a745"
+          text="页面数据加载中..." 
+          height="calc(100vh - 120px)" 
+        />
+      }>
         {renderRoutes(preparedRoutes)}
       </Suspense>
     </BrowserRouter>
