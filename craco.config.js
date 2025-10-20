3b37684100dfbf4898b20e3293b0a8cfa9c5278d
yxb <xiaobo.ye@hand-china.com>
2025-10-20 17:23:54 +0800

    add：添加gzip压缩

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const resolve = dir =>  path.resolve(__dirname, dir)
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   webpack: {
@@ -10,7 +11,15 @@ module.exports = {
     },
     plugins:[
       new BundleAnalyzerPlugin({analyzerPort: 9000}),
-    ], 
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: /.(js|css|html)$/, // 需要压缩的文件类型
+        threshold: 10240, // 只有大于 10KB 的文件才进行压缩
+        minRatio: 0.8, // 只有压缩比低于 0.8 才会被压缩
+        deleteOriginalAssets: false, // 是否删除原始文件，通常保留原始文件，方便回退
+      })
+    ],
+    mode: 'production',
     configure: (webpackConfig) => {
       webpackConfig.devtool = false;
       return webpackConfig;
